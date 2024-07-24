f6ce7989164abf961b5bb228e7413737879e3c0d
samabat.y <samabat.y@extend-it-resource.com>
2024-07-24 13:42:23 +0700

    follow

--- src/modules/to-do-list/to-do-list.service.ts
+++ src/modules/to-do-list/to-do-list.service.ts
@@ -1,11 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateToDoListDto } from './dto/create-to-do-list.dto';
 import { UpdateToDoListDto } from './dto/update-to-do-list.dto';
+import { CreateToDoListService } from 'src/usecases/to-do-list/create-to-do-list/create-to-do-list.service';
+import { DeleteToDoListService } from 'src/usecases/to-do-list/delete-to-do-list/delete-to-do-list.service';
+import { GetOneToDoListService } from 'src/usecases/to-do-list/get-one-to-do-list/get-one-to-do-list.service';
+import { UpdateToDoListService } from 'src/usecases/to-do-list/update-to-do-list/update-to-do-list.service';
+import { GetToDoListService } from 'src/usecases/to-do-list/get-to-do-list/get-to-do-list.service';
 
 @Injectable()
 export class ToDoListService {
+  constructor(
+    private readonly create_to_do_list_service: CreateToDoListService,
+    private readonly delete_to_do_list_service: DeleteToDoListService,
+    private readonly get_one_to_do_list_service: GetOneToDoListService,
+    private readonly get_to_do_list_service: GetToDoListService,
+    private readonly update_to_do_list_service: UpdateToDoListService,
+  ) {}
   create(createToDoListDto: CreateToDoListDto) {
-    return 'This action adds a new toDoList';
+    try {
+      return this.create_to_do_list_service.execute()
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
   }
 
   findAll() {
